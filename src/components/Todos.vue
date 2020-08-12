827945ed48b818c96988266fbcf73f682aa7d87e
Sree Sai Raghava <raghava.rym@gmail.com>
2020-08-12 14:35:04 +0530

    Initial version

---
+++ src/components/Todos.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="todos">
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+        {{todo.title}}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Todos",
+  methods: {
+    ...mapActions(["fetchTodos", "deleteTodo"]),
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  },
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #ffffff;
+  cursor: pointer;
+}
+</style>
